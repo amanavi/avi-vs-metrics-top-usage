5fb96238be36978b2d13cc0cd0751938e2327d5e
Amandeep Singh <37320515+amanavi@users.noreply.github.com>
2018-08-14 09:07:01 +0530

    Update vs_metrics_top.py

--- vs_metrics_top.py
+++ vs_metrics_top.py
@@ -5,12 +5,13 @@
 #
 # AVI-SDK based Script to get a list of VIPs using highest Bandwidth or no of Connections
 #
-# Requires AVI-SDK:  sudo pip install avisdk==17.2.11b1
+# Requires AVI-SDK of same version as Controller: e.g: sudo pip install avisdk==17.2.11b1
 # Real Time Metrics in AVI needs to be enabled for the VIPs
 #
 # Usage:-
 #  python vs_metrics_top.py -c <controller-ip> -u <user-name> -p <password> -t <tenant> -a <api-version> -l <limits> -s <step> -pm <parameter>
 #  python vs_metrics_top.py -c '10.140.196.5' -u 'admin' -p 'Admin@123' -t 'admin' -a '17.2.11' -l '10' -s '300' -pm 'bw'
+#  python vs_metrics_top.py -c '10.140.196.5' -u 'admin' -p 'Admin@123' -t 'admin' -a '17.2.11' -l '10' -s '300' -pm 'conn'
 #
 # step denotes the duration granularity of the metrics aggregation.
 # step=300 means the average, sum, max etc are computed using metrics in 5min duration window.
